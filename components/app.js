e13df27e62c9454826b73d259cdf72eea044f180
Robert Wilkinson <wilkinson.robert.a@gmail.com>
2016-01-10 17:08:44 -0800

    spinner

--- components/app.js
+++ components/app.js
@@ -31,36 +31,47 @@ export default class App extends Component {
     if(this.state.display) {
       display = this.state.selected.colors.map(color => <p> {'#' + color}</p>);
     }
-    return (
-      <div className="container">
-        <div className="row">
-          <div className="col-xs-12 text-center">
-            <h1>colorlovers</h1>
-            <p>Click a color, try it out</p>
-          </div>
-        </div>
-        <div className="row">
-            <div className='col-xs-6 tk-source-sans-pro' >
-              <PalleteList
-                handleClick={(pallete) => {
-                  this.setState({selected: pallete})
-                }}
-                palletes={this.state.palletes}
-              />
+    if(this.state.palletes.length){
+      return (
+        <div className="container">
+          <div className="row">
+            <div className="col-xs-12 text-center">
+              <h1>colorlovers</h1>
+              <p>Click a color, try it out</p>
             </div>
-            <div className='col-xs-6'>
-                  {selected}
+          </div>
+          <div className="row">
+              <div className='col-xs-6 tk-source-sans-pro' >
+                <PalleteList
+                  handleClick={(pallete) => {
+                    this.setState({selected: pallete})
+                  }}
+                  palletes={this.state.palletes}
+                />
+              </div>
+              <div className='col-xs-6'>
+                    {selected}
+              </div>
+          </div>
+          <div className="row" >
+            <div className="col-xs-6" >
+              Thanks for trying it out
+              <br />
+              made by <a href="http://twitter.com/robawilkinson" >@robawilkinson</a>
             </div>
         </div>
-        <div className="row" >
-          <div className="col-xs-6" >
-            Thanks for trying it out
-            <br />
-            made by <a href="http://twitter.com/robawilkinson" >@robawilkinson</a>
-          </div>
       </div>
-    </div>
-    )
+      )
+    }
+    return (
+        <div className="row">
+          <div className="col-xs-12">
+            <img style={{margin: '0 auto'}} src="spinner.gif">
+            </img>
+          </div>
+        </div>
+        );
+
   }
 }
 
